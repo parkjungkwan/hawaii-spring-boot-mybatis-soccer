446e2b0577301c9aa3576391a4c0014b5abc5927
pakjkwan <pakjkwan@gmail.com>
2020-05-13 15:11:41 +0900

    Initial commit

---
+++ src/main/java/com/parksrazor/web/HomeController.java
@@ -0,0 +1,12 @@
+package com.parksrazor.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "반갑다 스프링부트 !!";
+    }
+}
---
+++ src/main/java/com/parksrazor/web/domains/PlayerDTO.java
@@ -0,0 +1,7 @@
+package com.parksrazor.web.domains;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlayerDTO {
+}
